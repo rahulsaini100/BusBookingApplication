861ba66f5e90773cb87dcc59cf54b7299502f31e
Rahul Saini <rahulsaini85420@gmail.com>
2021-07-28 10:13:23 +0530

    Created seat entity

---
+++ Bus Booking Project/src/main/java/com/timesinternet/busbooking/Entities/Seat.java
@@ -0,0 +1,43 @@
+package com.timesinternet.busbooking.Entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+
+@Entity
+@Table
+public class Seat {
+
+	@Id
+	@Column(length = 5)
+	private String busId;
+	private Long totalSeat;
+	
+	public Seat() {
+		
+	}	
+    public Seat(String busId, Long totalSeat) {
+		super();
+		this.busId = busId;
+		this.totalSeat= totalSeat;
+	}
+	public String getBusId() {
+		return busId;
+	}
+	public void setBusId(String busId) {
+		this.busId = busId;
+	}
+	
+	public Long getTotalSeat() {
+		return totalSeat;
+	}
+	public void setTotalSeat(Long totalSeat) {
+		this.totalSeat = totalSeat;
+	}
+	
+     
+	 
+	
+}
