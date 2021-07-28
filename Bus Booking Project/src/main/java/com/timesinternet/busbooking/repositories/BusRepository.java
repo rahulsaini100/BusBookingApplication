2f6679ba23659e3be38ff398ba1ae9f6aec36439
Rahul Saini <rahulsaini85420@gmail.com>
2021-07-28 18:53:49 +0530

    Search API Created

--- Bus Booking Project/src/main/java/com/timesinternet/busbooking/repositories/BusRepository.java
+++ Bus Booking Project/src/main/java/com/timesinternet/busbooking/repositories/BusRepository.java
@@ -14,7 +14,7 @@ import java.util.List;
 @Repository
 public interface BusRepository extends JpaRepository<Bus,Long> {
 
-    @Query("SELECT new com.timesinternet.busbooking.Entities.abc(b.busId,r.arrivalTime,r.departureTime) \n"
+    @Query("SELECT new com.timesinternet.busbooking.entities.abc(b.busId,r.arrivalTime,r.departureTime) \n"
     		+ "from Bus as b INNER JOIN Route as r on b.routeId=r.routeId\n"
     		+ "WHERE r.fromCityId=(SELECT cityId from City where cityname=?1)\n "
     		+ "and r.toCityId=(SELECT cityId from City where cityname=?2)")
