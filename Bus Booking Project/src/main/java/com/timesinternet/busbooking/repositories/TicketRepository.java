d55db2ec74736c1ee05509981b8b2480ee5b5fb0
Vikas Sahani <vikas.sahani@timesinternet.in>
2021-08-11 14:25:24 +0530

    ticket repository

--- Bus Booking Project/src/main/java/com/timesinternet/busbooking/repositories/TicketRepository.java
+++ Bus Booking Project/src/main/java/com/timesinternet/busbooking/repositories/TicketRepository.java
@@ -22,15 +22,15 @@ public interface TicketRepository extends JpaRepository<Ticket,Long> {
 //    
 //    List<abc>FindRoute(String fromCityName, String toCityName);
 	
-	@Query("select new com.timesinternet.busbooking.entities.Ticket(t.bookingId,t.busId,t.userId,t.routeId,t.numberOfSeats,t.dateOfBooking,t.journeyDate) \n"
-			+ "from Ticket as t where t.userId=?1")
+//	@Query("select new com.timesinternet.busbooking.entities.Ticket(t.bookingId,t.busId,t.userId,t.routeId,t.numberOfSeats,t.dateOfBooking,t.journeyDate) \n"
+//			+ "from Ticket as t where t.userId=?1")
 	
-//	@Query("select new com.timesinternet.busbooking.entities.GenerateTicket(t.userId,u.userName,"
-//			+ " u.userPhoneNumber,u.userAddress,t.busId,(select cityName from City where cityId=r.fromCityId),"
-//			+ "(select cityName from City where cityId=r.toCityId),t.dateOfBooking,t.journeyDate,t.numberOfSeats,"
-//			+ "p.TicketPrice*t.numberOfSeats)" + " from Ticket as t INNER join Users as u on t.userId=u.userId "
-//			+ "INNER JOIN Route as r on t.routeId=r.routeId " + "INNER JOIN Price as p on r.routeId=p.routeId "
-//			+ "where t.bookingId=?1")
+	@Query("select new com.timesinternet.busbooking.entities.GenerateTicket(t.userId,u.userName,"
+			+ " u.userPhoneNumber,u.userAddress,t.busId,(select cityName from City where cityId=r.fromCityId),"
+			+ "(select cityName from City where cityId=r.toCityId),t.dateOfBooking,t.journeyDate,t.numberOfSeats,"
+			+ "p.TicketPrice*t.numberOfSeats)" + " from Ticket as t INNER join Users as u on t.userId=u.userId "
+			+ "INNER JOIN Route as r on t.routeId=r.routeId " + "INNER JOIN Price as p on r.routeId=p.routeId "
+			+ "where t.bookingId=?1")
 	
 	
 			
