a7607c52654a43bbd1637d044011577dfb18c36b
Vikas Sahani <vikas.sahani@timesinternet.in>
2021-08-24 21:59:26 +0530

    validation on userPhoneNumber in mybooking API

--- Bus Booking Project/src/main/java/com/timesinternet/busbooking/controllers/MyBookingController.java
+++ Bus Booking Project/src/main/java/com/timesinternet/busbooking/controllers/MyBookingController.java
@@ -1,12 +1,16 @@
 package com.timesinternet.busbooking.controllers;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
 import com.timesinternet.busbooking.entities.GenerateTicket;
 import com.timesinternet.busbooking.repositories.TicketRepository;
 
@@ -36,11 +40,20 @@ public class MyBookingController {
 
 	/**
 	 * this endpoint takes input userPhoneNumber and returns list of Tickets booked over that input
+	 * if no booking is there for that number than it wi return error 400
 	 * @param userPhoneNumber
 	 * @return
 	 */
 	@PostMapping(value = "/mybooking")
 	public List<GenerateTicket> myBookingFn(@RequestParam String userPhoneNumber) {
+		
+//		List<GenerateTicket> len = new ArrayList<GenerateTicket>();
+//		
+//		len = serviceLayer.showBooking(userPhoneNumber);
+//		if(len.size() == 0) {
+//			throw new ResponseStatusException(HttpStatus.BAD_REQUEST,
+//					"No booking for this number");
+//		}
 		
 		return serviceLayer.showBooking(userPhoneNumber);
 
